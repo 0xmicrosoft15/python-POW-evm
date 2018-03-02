ethereum/py-evm

Added `ShardChain` and `ShardVM`

evm/vm/base.py
@@ -41,7 +41,7 @@ from .execution_context import (
 )
 
 
-class VM(object):
+class BaseVM(object):
     """
     The VM class represents the Chain rules for a specific protocol definition
     such as the Frontier or Homestead network.  Defining an Chain  defining

0xmicrosoft15/python-POW-evm

evm/vm/base.py
@@ -44,7 +44,7 @@ from .execution_context import (
 )
 
 
-class VM(Configurable):
+class BaseVM(Configurable):
     """
     The VM class represents the Chain rules for a specific protocol definition
     such as the Frontier or Homestead network.  Define a Chain which specifies