ethereum/py-evm

Allow plugins to run in their own processes

trinity/extensibility/plugin_manager.py
@@ -4,9 +4,16 @@ from argparse import (
 )
 import logging
 from typing import (
+    Any,
+    Dict,
     Iterable,
     List,
     Union,
+)
+
+from lahja import (
+    Endpoint,
+    EventBus,
 )
 
 from trinity.extensibility.events import (
@@ -15,7 +22,25 @@ from trinity.extensibility.events import (
 )
 from trinity.extensibility.plugin import (
     BasePlugin,
+    PluginContext,
+    PluginProcessScope,
 )
+
+
+class MainAndOwnProcessScope():
+
+    def __init__(self, event_bus: EventBus, main_proc_endpoint: Endpoint) -> None:
+        self.event_bus = event_bus
+        self.endpoint = main_proc_endpoint
+
+
+class SharedProcessScope():
+
+    def __init__(self, shared_proc_endpoint: Endpoint) -> None:
+        self.endpoint = shared_proc_endpoint
+
+
+ManagerProcessScope = Union[SharedProcessScope, MainAndOwnProcessScope]
 
 
 class PluginManager:
@@ -28,10 +53,18 @@ class PluginManager:
         This API is very much in flux and is expected to change heavily.
     """
 
-    def __init__(self) -> None:
+    def __init__(self, scope: ManagerProcessScope) -> None:
+        self._scope = scope
         self._plugin_store: List[BasePlugin] = []
         self._started_plugins: List[BasePlugin] = []
         self._logger = logging.getLogger("trinity.extensibility.plugin_manager.PluginManager")
+
+    @property
+    def event_bus_endpoint(self) -> Endpoint:
+        """
+        Return the ``Endpoint`` that the ``PluginManager`` uses to connect to the ``EventBus``
+        """
+        return self._scope.endpoint
 
     def register(self, plugins: Union[BasePlugin, Iterable[BasePlugin]]) -> None:
         """
@@ -63,7 +96,7 @@ class PluginManager:
         """
         for plugin in self._plugin_store:
 
-            if plugin is exclude:
+            if plugin is exclude or not self._is_responsible_for_plugin(plugin):
                 continue
 
             plugin.handle_event(event)
@@ -74,7 +107,53 @@ class PluginManager:
             if not plugin.should_start():
                 continue
 
-            plugin.start(None)
+            context = self._create_context_for_plugin(plugin)
+            plugin.start(context)
             self._started_plugins.append(plugin)
             self._logger.info("Plugin started: {}".format(plugin.name))
             self.broadcast(PluginStartedEvent(plugin), plugin)
+
+    # FIXME: This is a temporary API
+    def start(self, boot_kwargs: Dict[str, Any]) -> None:
+        for plugin in self._plugin_store:
+
+            # This whole API is going away so for now, we aren't caring about
+            # any of the other plugins
+
+            if plugin.process_scope is not PluginProcessScope.OWN:
+                continue
+
+            context = self._create_context_for_plugin(plugin, boot_kwargs)
+            plugin.start(context)
+
+    def _is_responsible_for_plugin(self, plugin: BasePlugin) -> bool:
+
+        main_and_own_scopes = [PluginProcessScope.MAIN, PluginProcessScope.OWN]
+        plugin_for_main_or_own = plugin.process_scope in main_and_own_scopes
+        plugin_for_shared = not plugin_for_main_or_own
+
+        manager_for_main_or_own = isinstance(self._scope, MainAndOwnProcessScope)
+        manager_for_shared = not manager_for_main_or_own
+
+        return (plugin_for_main_or_own and manager_for_main_or_own or
+                plugin_for_shared and manager_for_shared)
+
+    def _create_context_for_plugin(self,
+                                   plugin: BasePlugin,
+                                   boot_kwargs: Dict[str, Any] = None) -> PluginContext:
+
+        if plugin.process_scope is PluginProcessScope.SHARED:
+            # A plugin that runs in a shared process gets the endpoint of the PluginManager
+            # injected to communicate with the rest of the world
+            return PluginContext(self._scope.endpoint)
+        elif plugin.process_scope is PluginProcessScope.OWN:
+            # A plugin that runs in it's own process gets a new endpoint created to get
+            # passed into that new process
+
+            # mypy doesn't know it can only be that scope at this point. The `isinstance`
+            # check avoids adding an ignore
+            if isinstance(self._scope, MainAndOwnProcessScope):
+                endpoint = self._scope.event_bus.create_endpoint(plugin.name)
+                return PluginContext(endpoint, boot_kwargs)
+
+        raise Exception("Invariant: unreachable code path")

0xmicrosoft15/python-POW-evm

trinity/extensibility/plugin_manager.py
@@ -1,21 +1,50 @@
 from argparse import (
     ArgumentParser,
+    Namespace,
     _SubParsersAction,
 )
 import logging
 from typing import (
+    Any,
+    Dict,
     Iterable,
     List,
     Union,
 )
 
+from lahja import (
+    Endpoint,
+    EventBus,
+)
+
+from trinity.config import (
+    ChainConfig
+)
 from trinity.extensibility.events import (
     BaseEvent,
     PluginStartedEvent,
 )
 from trinity.extensibility.plugin import (
     BasePlugin,
+    PluginContext,
+    PluginProcessScope,
 )
+
+
+class MainAndIsolatedProcessScope():
+
+    def __init__(self, event_bus: EventBus, main_proc_endpoint: Endpoint) -> None:
+        self.event_bus = event_bus
+        self.endpoint = main_proc_endpoint
+
+
+class SharedProcessScope():
+
+    def __init__(self, shared_proc_endpoint: Endpoint) -> None:
+        self.endpoint = shared_proc_endpoint
+
+
+ManagerProcessScope = Union[SharedProcessScope, MainAndIsolatedProcessScope]
 
 
 class PluginManager:
@@ -28,10 +57,21 @@ class PluginManager:
         This API is very much in flux and is expected to change heavily.
     """
 
-    def __init__(self) -> None:
+    MAIN_AND_ISOLATED_SCOPES = {PluginProcessScope.MAIN, PluginProcessScope.ISOLATED}
+    MAIN_ANS_SHARED_SCOPES = {PluginProcessScope.MAIN, PluginProcessScope.SHARED}
+
+    def __init__(self, scope: ManagerProcessScope) -> None:
+        self._scope = scope
         self._plugin_store: List[BasePlugin] = []
         self._started_plugins: List[BasePlugin] = []
         self._logger = logging.getLogger("trinity.extensibility.plugin_manager.PluginManager")
+
+    @property
+    def event_bus_endpoint(self) -> Endpoint:
+        """
+        Return the ``Endpoint`` that the ``PluginManager`` uses to connect to the ``EventBus``
+        """
+        return self._scope.endpoint
 
     def register(self, plugins: Union[BasePlugin, Iterable[BasePlugin]]) -> None:
         """
@@ -63,7 +103,7 @@ class PluginManager:
         """
         for plugin in self._plugin_store:
 
-            if plugin is exclude:
+            if plugin is exclude or not self._is_responsible_for_plugin(plugin):
                 continue
 
             plugin.handle_event(event)
@@ -74,7 +114,67 @@ class PluginManager:
             if not plugin.should_start():
                 continue
 
-            plugin.start(None)
+            plugin._start()
             self._started_plugins.append(plugin)
             self._logger.info("Plugin started: {}".format(plugin.name))
             self.broadcast(PluginStartedEvent(plugin), plugin)
+
+    def prepare(self,
+                args: Namespace,
+                chain_config: ChainConfig,
+                boot_kwargs: Dict[str, Any] = None) -> None:
+        for plugin in self._plugin_store:
+
+            if not self._is_responsible_for_plugin(plugin):
+                continue
+
+            context = self._create_context_for_plugin(plugin, args, chain_config, boot_kwargs)
+            plugin.set_context(context)
+
+    def shutdown(self) -> None:
+        for plugin in self._started_plugins:
+            try:
+                plugin.stop()
+            except Exception:
+                self._logger.exception("Exception thrown while stopping plugin %s", plugin.name)
+
+    def _is_responsible_for_plugin(self, plugin: BasePlugin) -> bool:
+
+        main_or_isolated_plugin = plugin.process_scope in self.MAIN_AND_ISOLATED_SCOPES
+        shared_plugin = not main_or_isolated_plugin
+
+        manager_for_main_or_isolated = isinstance(self._scope, MainAndIsolatedProcessScope)
+        manager_for_shared = not manager_for_main_or_isolated
+
+        return ((main_or_isolated_plugin and manager_for_main_or_isolated) or
+                (shared_plugin and manager_for_shared))
+
+    def _create_context_for_plugin(self,
+                                   plugin: BasePlugin,
+                                   args: Namespace,
+                                   chain_config: ChainConfig,
+                                   boot_kwargs: Dict[str, Any]) -> PluginContext:
+
+        context: PluginContext = None
+        if plugin.process_scope in self.MAIN_ANS_SHARED_SCOPES:
+            # A plugin that runs in a shared process as well as a plugin that overtakes the main
+            # process uses the endpoint of the PluginManager which will either be the main
+            # endpoint or the networking endpoint in the case of Trinity
+            context = PluginContext(self._scope.endpoint)
+        elif plugin.process_scope is PluginProcessScope.ISOLATED:
+            # A plugin that runs in it's own process gets a new endpoint created to get
+            # passed into that new process
+
+            # mypy doesn't know it can only be that scope at this point. The `isinstance`
+            # check avoids adding an ignore
+            if isinstance(self._scope, MainAndIsolatedProcessScope):
+                endpoint = self._scope.event_bus.create_endpoint(plugin.name)
+                context = PluginContext(endpoint)
+        else:
+            Exception("Invariant: unreachable code path")
+
+        context.args = args
+        context.chain_config = chain_config
+        context.boot_kwargs = boot_kwargs
+
+        return context